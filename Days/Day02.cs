6243e3113c8d258ff7897d77b82a9b3a0f805bc2
Tomas Vaedelund <tvaedelund@gmail.com>
2018-12-02 07:26:14 +0100

    Day02 - 1

--- Days/Day02.cs
+++ Days/Day02.cs
@@ -10,12 +10,12 @@ namespace Advent_of_Code_2018.Days
         public static void GetResult()
         {
             // First star
-            Debug.Assert(true == true);
+            Debug.Assert(GetChecksumPart("abcdef, bababc, abbcde, abcccd, aabcdd, abcdee, ababab", ",") == 12);
 
             var data = Helpers.GetDataFromFile("day02.txt");
             var result = "";
             var stopWatch = Stopwatch.StartNew();
-            result = "".ToString();
+            result = GetChecksumPart(data).ToString();
             Helpers.DisplayDailyResult("02 - 1", result, stopWatch.ElapsedMilliseconds);
 
             // Second star
@@ -25,6 +25,26 @@ namespace Advent_of_Code_2018.Days
             result = "".ToString();
             Helpers.DisplayDailyResult("02 - 2", result, stopWatch.ElapsedMilliseconds);
             stopWatch.Stop();
+        }
+
+        private static int GetChecksumPart(string data, string splitter = "\r\n")
+        {
+            var dataArray = data.Split(splitter);
+            var counter = new int[dataArray.First().Length];
+
+            foreach (var item in dataArray)
+            {
+                item
+                .GroupBy(c => c)
+                .Select(c => new { Char = c.Key, Count = c.Count() })
+                .Where(c => c.Count > 1)
+                .Select(c => c.Count)
+                .Distinct()
+                .ToList()
+                .ForEach(c => counter[c]++);
+            }
+
+            return counter.Where(c => c > 0).Aggregate((c, n) => c * n);
         }
     }
 }
