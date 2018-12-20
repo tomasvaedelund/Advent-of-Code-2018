cba27215664c920f13daf0969dcd56f1646e10ca
Vaedelund <tomas.vaedelund@abetong.se>
2018-12-20 07:50:50 +0100

    Prepared Day20

---
+++ 2018/Day20/Solution.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace AdventOfCode.Y2018.Day20
+{
+    class Solution : Solver
+    {
+        public string GetName() => "Day20";
+
+        public void Test(string input) => new Test().Run(input);
+
+        public IEnumerable<(string result, long time)> Solve(string input)
+        {
+            yield return PartOne(input);
+            yield return PartTwo(input);
+        }
+
+        public (string result, long time) PartOne(string input)
+        {
+            var timer = Stopwatch.StartNew();
+            return ($"result", timer.ElapsedMilliseconds);
+        }
+
+        public (string result, long time) PartTwo(string input)
+        {
+            var timer = Stopwatch.StartNew();
+            return ($"result", timer.ElapsedMilliseconds);
+        }
+    }
+}
